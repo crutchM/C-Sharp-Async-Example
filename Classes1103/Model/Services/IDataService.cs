536310b5fe80bfbf7aa400d53ac5a642745b1fac
crutchM <sads1100@gmail.com>
2023-03-11 16:25:41 +0500

    Added xmldocs

--- Classes1103/Model/Services/IDataService.cs
+++ Classes1103/Model/Services/IDataService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -5,9 +6,37 @@ namespace Classes1103.Model;
 
 public interface IDataService<T>
 {
+    /// <summary>
+    /// Returns all <typeparamref name="T"/>
+    /// contained within this <see cref="IAsyncDataService{T}"/>.
+    /// </summary>
+    /// <returns></returns>
     public IEnumerable<T> FindAll();
+    /// <summary>
+    /// Returns <typeparamref name="T"/> associated with
+    /// <paramref name="id"/> or <see langword="null"/>
+    /// if it does not exist.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public T? Find(int id);
+    /// <summary>
+    /// Adds and saves <paramref name="data"/>.
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
     public void Add(T data);
+    /// <summary>
+    /// Updates <paramref name="data"/> or throws <see cref="InvalidOperationException"/>
+    /// if it is not present.
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
     public void Update(T data);
+    /// <summary>
+    /// Deletes <paramref name="data"/> if it exists.
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
     public void Delete(T data);
 }
